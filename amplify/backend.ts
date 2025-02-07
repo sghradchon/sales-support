c6050672200fb50b36abb54bbe2561223d409288
sghradchon <sghradchon@gmail.com>
2025-02-07 12:09:25 +0900

    lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { makeslideFunctionHandler } from './function/makeslide/resource';
 
 defineBackend({
   auth,
   data,
+  makeslideFunctionHandler
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { makeslideFunctionHandler } from "../function/makeslide/resource";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -12,7 +13,18 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
-});
+      
+  // makeslide: a
+  // .query()
+  // .arguments({
+  //   name: a.string(),
+  // })
+  // .returns(a.string())
+  // .handler(a.handler.function(makeslideFunctionHandler)),
+
+
+}
+);
 
 export type Schema = ClientSchema<typeof schema>;
 
