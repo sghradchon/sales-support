5f73b67006a783c37a9a7adb341833aaf5daa1c0
sghradchon <sghradchon@gmail.com>
2025-03-21 15:04:11 +0900

    s

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { Box, Button, Flex } from '@chakra-ui/react';
 import SalesHeatMapPage from './pages/SalesHeatMapPage';
 import ProductSlideCreation from './pages/ProductSlideCreation';
-
+import TextsClustering from './pages/TextsClustering';
 
 const App: React.FC = () => {
   return (
@@ -12,17 +12,21 @@ const App: React.FC = () => {
         {/* メニューバー */}
         <Flex bg="teal.500" p={4} justifyContent="space-around">
         <Link to="/ProductSlidePage">
-            <Button colorScheme="red" variant="outline">MAKE SLIDE</Button>
+            <Button colorScheme="teal" variant="outline">MAKE SLIDE</Button>
           </Link>
           <Link to="/salesHeatMapPage">
             <Button colorScheme="teal" variant="outline">SALES HEATMAP</Button>
           </Link>
+          <Link to="/TextsClustering">
+            <Button colorScheme="teal" variant="outline">Texts Clustering</Button>
+          </Link>
+
         </Flex>
         {/* ルーティングの設定 */}
         <Routes>
           <Route path="/ProductSlidePage" element={<ProductSlideCreation />} />
           <Route path="/salesHeatMapPage" element={<SalesHeatMapPage />} />
-
+          <Route path="/TextsClustering" element={<TextsClustering />} />
         </Routes>
       </Box>
     </Router>
